8683c9dfb843b723e6802b5668d14ba63c8b5af2
Pascal Chambon <chambon.pascal@gmail.com>
2016-07-01 14:10:15 +0200

    Disable asset compression for now, to help debugging.

--- philosophy/settings/base.py
+++ philosophy/settings/base.py
@@ -27,7 +27,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True  # set it to False when loading data from sqlite...
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
@@ -192,7 +192,7 @@ THUMBNAIL_PROCESSORS = (
 
 ## Django-compressor settings
 
-COMPRESS_ENABLED = True
+COMPRESS_ENABLED = False
 
 COMPRESS_PRECOMPILERS = (
     # unused('text/x-scss', 'django_libsass.SassCompiler'),
